f70a2f957a2c95292d02945bd1a51bcfbde9aabf
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-10 00:11:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(sentence)
   rev_array = array.map{|i| i.reverse}
   rev_array.join(" ")
 end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  rev_array = array.map{|i| i.reverse}
+  rev_array.join(" ")
+end
