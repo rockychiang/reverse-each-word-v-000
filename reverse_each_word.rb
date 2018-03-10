9b21ae3bf0d69baaa69eeb7f0f642e897cce82df
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-10 00:11:36 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  rev_array = array.map{|i| i.reverse}
+  rev_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  rev_array = array.collect{|i| i.reverse}
+  rev_array.join(" ")
+end
